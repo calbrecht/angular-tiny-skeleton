1294c145174d62105146c579bda69011d82eb7f6
Christian Albrecht <christian.albrecht@mayflower.de>
2014-01-18 07:41:44 +0100

    Increase (dependencies-)version

--- specs/e2e.karma.conf.js
+++ specs/e2e.karma.conf.js
@@ -1,13 +1,18 @@
 // basePath defined in Gruntfile.js
-files = [
-    'node_modules/karma/adapter/lib/angular-scenario.js',
-    'node_modules/karma/adapter/angular-scenario.js',
-    //watch for reload but do not include into html
-    {pattern: '**/*.html', included: false},
-    {pattern: '**/*.css', included: false},
-    'lib/angular/angular.js',
-    'lib/**/*.js',
-    'src/**/*.js',
-    'specs/e2e/**/*.js'
-];
-
+module.exports = function(config) {
+    config.set({
+        frameworks: ['ng-scenario'],
+        preprocessors: {
+            '**/*.html': []
+        },
+        files: [
+            //watch for reload but do not include into html
+            {pattern: '**/*.html', included: false},
+            {pattern: '**/*.css', included: false},
+            'lib/angular/angular.js',
+            'lib/**/*.js',
+            'src/**/*.js',
+            'specs/e2e/**/*.js'
+        ]
+    });
+};
--- specs/unit.karma.conf.js
+++ specs/unit.karma.conf.js
@@ -1,13 +1,15 @@
 //basePath defined in Gruntfile.js
 
-files = [
-    'node_modules/karma/adapter/lib/jasmine.js',
-    'node_modules/karma/adapter/jasmine.js',
-    //watch for reload but do not include into html
-    {pattern: '**/*.html', included: false},
-    'lib/angular/angular.js',
-    'lib/**/*.js',
-    'src/**/*.js',
-    'specs/unit/**/*.js'
-];
-
+module.exports = function(config) {
+    config.set({
+        frameworks: ['jasmine'],
+        files: [
+            //watch for reload but do not include into html
+            {pattern: '**/*.html', included: false},
+            'lib/angular/angular.js',
+            'lib/**/*.js',
+            'src/**/*.js',
+            'specs/unit/**/*.js'
+        ]
+    });
+};
